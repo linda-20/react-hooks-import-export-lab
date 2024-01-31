d37def4586ae9f3a8ae173038e51088a27c6a24a
linda-20 <lindaaron40@gmail.com>
2024-01-31 17:42:43 +0300

    Completed asssignment

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,7 +5,7 @@ import App from "../components/App";
 test("it renders without crashing", () => {
   try {
     expect(() => render(<App />)).not.toThrow();
-  } catch (e) {
-    throw new Error("Make sure to import the required components!");
+  } catch (error) {
+    throw new Error(`Failed to render App component: ${error.message}`);
   }
 });
--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -1,3 +1,4 @@
+// user.test.js
 import { username, city, image } from "../data/user";
 
 test("username, city, and image are all exported", () => {
